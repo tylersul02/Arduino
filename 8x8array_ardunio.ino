7389a6e0672e95f7421619f2c0d3a4dc91f9387a
tylersul02 <42984907+tylersul02@users.noreply.github.com>
2019-04-18 10:07:41 -0700

    Update 8x8array_ardunio.ino

--- 8x8array_ardunio.ino
+++ 8x8array_ardunio.ino
@@ -1,45 +1,34 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
-// Modified by CraigColeman  for an 8x8 neopixel
+
 int clearPix ();
-int cwc ();
+int trs ();
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
 #define PIN            6
 
-// How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      64
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-//  8x8 array
 
 int i = 0, j = 0;
 
-
-//
-int delayval = 50; // delay for half a second
+int delayval = 50; 
 
 void setup() {
-  Serial.begin(9600);      //ser up Serial.*
+  Serial.begin(9600);
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
   // End of trinket special code
 
-  pixels.begin(); // This initializes the NeoPixel library.
+  pixels.begin();
 }
 
 void loop() {
-clearPix(); delay(1000); cwc();  delay(2000);
+clearPix(); delay(1000); trs();  delay(2000);
  
   
 }
@@ -50,9 +39,8 @@ int clearPix (){
     pixels.show();
   }
 
-int cwc(){
-//ser base color
-int r,c;//row and column
+int trs(){
+int r,c;
 int red = 0, green = 0, blue = 0;
 int m[8][8] = {  
     {0,0,0,0,0,0,0,0} ,
@@ -64,12 +52,10 @@ int m[8][8] = {
     {0,1,1,1,1,1,1,0},
     {0,0,0,0,0,0,0,0}   
 };
-  i = 0;// set neo-pixel number to zero
+  i = 0;
   int val;
-//  loop that increments x,y and i
   for (r = 0; r < 8; r++){
     for (c = 0; c < 8; c++){
-      // set the color for red green and blue
       val = m[r][c];
       switch ( val ) {
           case 0:
